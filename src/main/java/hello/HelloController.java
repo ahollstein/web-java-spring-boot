6003897477b8ee0e4ecb19959629d9b84f05c7c9
ahollstein <a.hollstein@iubh-fernstudium.de>
2019-06-22 12:41:05 +0000

    My first requestmapper

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,6 +2,8 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HelloController {
@@ -10,5 +12,18 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(@RequestParam(required = false) String name) {
+        if (name == null) {
+            return "<h1>Hallo Welt, here I am! </h1>";
+            }
+        else{
+            return "<h1>Hallo Welt, here I am, " + name + " the cool!</h1>";
+            }
+        
+    }
+
     
 }
